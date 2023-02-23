297cc54005181b0336f93f812b399e775576f761
Andrew Hawileh <andrewh829@gmail.com>
2023-02-23 15:40:13 -0600

    done

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const button = document.getElementById('button')
+    button.addEventListener('click', (e) => alert('I was clicked!'))
 }
+
+addingEventListener();
